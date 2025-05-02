588d71d1fc21512981c7a65976a58bf9a907f0d1
sethbtaylor2020 <sethbtaylor@byui.edu>
2025-05-02 11:17:32 -0600

    assignmentcompleted

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+         Console.Write("What was your final grade for the class?(number) ");
+        string letter_grade = Console.ReadLine();
+        int grade = int.Parse(letter_grade);
+
+         if (grade >= 90)
+        Console.WriteLine ("You got an A in this class.");
+
+       else if (grade >= 80)
+        Console.WriteLine ("You got an B in this class.");
+
+        else if (grade >= 70)
+        Console.WriteLine ("You got an C in this class.");
+
+        else if (grade >= 60)
+        Console.WriteLine ("You got an D in this class.");
+
+        else
+        Console.WriteLine ("You failed this course.");
     }
 }
