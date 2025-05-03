0b77b3347d8fb280b2744f106ef71947e2831a7d
sethbtaylor2020 <sethbtaylor@byui.edu>
2025-05-03 17:52:01 -0600

    test

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,33 @@
 using System;
+using System.Security.Cryptography;
 
 class Program
 {
     static void Main(string[] args)
+    
+
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random magicnumber = new Random();
+        int number = magicnumber.Next(1,11);
+
+        int guess = -1;
+
+        while (guess != number)
+
+        {
+            Console.Write("Guess the magic number. ");
+            guess = int.Parse(Console.ReadLine());
+
+                if (guess > number)
+            Console.WriteLine ("Lower");
+
+                else if (guess < number)
+            Console.WriteLine ("Higher");
+        }
+
+        {
+            Console.WriteLine ("You Guessed it!");
+        }
+        
     }
 }
