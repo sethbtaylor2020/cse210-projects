9bbac0b08d78e7c524906ee4927c286ec54978ea
sethbtaylor2020 <sethbtaylor@byui.edu>
2025-05-03 21:43:51 -0600

    squared

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,19 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        Console.WriteLine("Welcome to the Program");
+        Console.WriteLine("What is your name?");
+        string username = Console.ReadLine();
+
+        Console.WriteLine("Whats your favorite number?");
+         int favoritenumber = int.Parse(Console.ReadLine());
+
+    
+    //Squaring stuff starts :'( //
+
+    
+        int square = favoritenumber * favoritenumber;
+        Console.WriteLine($"{username} Your Favorite Number Squared is: {square}");
+
     }
 }
