9813e51d8e803e75bc69c9698174ccaac26f422c
sethbtaylor2020 <sethbtaylor@byui.edu>
2025-05-22 23:49:08 -0600

    final

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,19 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        //commanded fractions?
+        Fraction f1 = new Fraction();
+        Fraction f2 = new Fraction(5);
+        Fraction f3 = new Fraction(3, 4);
+        Fraction f4 = new Fraction(1, 3);
+
+        // Outputs for commanded fractions
+        Console.WriteLine(f1.GetFractionsString());
+        Console.WriteLine(f2.GetFractionsString());
+        Console.WriteLine(f3.GetFractionsString());
+        Console.WriteLine(f4.GetFractionsString());
+        f1.SetTop(6);
+        f1.SetBottom(7);
+        Console.WriteLine(f1.GetFractionsString());
     }
 }
